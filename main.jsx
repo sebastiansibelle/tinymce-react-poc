589a5d1926992c72b3aa2d45b4ca25bea9ce75f5
Sebastian Sibelle <sebastian@chuffed.org>
2016-05-13 10:24:17 +1000

    initial commits

---
+++ main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TinyMCE from 'react-tinymce';
+
+const App = React.createClass({
+  handleEditorChange(e) {
+    console.log('Content was updated:', e.target.getContent());
+  },
+
+  render() {
+    return (
+      <TinyMCE
+        content="<p>This is the initial content of the editor</p>"
+        config={{
+          plugins: 'link image code',
+          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+        }}
+        onChange={this.handleEditorChange}
+      />
+    );
+  }
+});
+
+ReactDOM.render(<App/>, document.getElementById('container'));
